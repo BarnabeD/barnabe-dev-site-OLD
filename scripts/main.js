299db24dd0881b7ce42937e44655244fff2a2596
Barnabe DUBUS <barnabe.dubus@gmail.com>
2021-02-05 20:08:56 +0100

    front stage 1 ok

--- scripts/main.js
+++ scripts/main.js
@@ -4,30 +4,13 @@ const options = document.querySelectorAll('#theme-select > li')
 const root = document.documentElement
 select.addEventListener('click', (event) => {
   if (event.currentTarget.classList.contains('colapsed')) {
-    openMenu()
+    openThemeMenu()
     event.currentTarget.classList.remove('colapsed')
   } else {
-    closeMenu()
+    closeThemeMenu()
+    closeHamburgerMenu()
   }
 })
-
-const themeChanger = (themeName) => {
-  setBackgroundColor(themes[themeName].background)
-  setTextColor(themes[themeName].text)
-  setBorderColor(themes[themeName].border)
-}
-
-const setBackgroundColor = (color) => {
-  root.style.setProperty('--theme-background-color', color)
-}
-
-const setTextColor = (color) => {
-  root.style.setProperty('--theme-text-color', color)
-}
-
-const setBorderColor = (color) => {
-  root.style.setProperty('--theme-border-color', color)
-}
 
 options.forEach((opt) => {
   opt.addEventListener('click', (event) => {
@@ -40,14 +23,14 @@ options.forEach((opt) => {
   })
 })
 
-const openMenu = () => {
+const openThemeMenu = () => {
   options.forEach((opt) => {
     opt.classList.replace('hidden', 'visible')
     opt.classList.remove('selected')
   })
 }
 
-const closeMenu = () => {
+const closeThemeMenu = () => {
   options.forEach((opt) => {
     if (!opt.classList.contains('selected')) {
       opt.classList.replace('visible', 'hidden')
@@ -56,20 +39,95 @@ const closeMenu = () => {
   select.classList.add('colapsed')
 }
 
+const themeChanger = (themeName) => {
+  const elements = ['background', 'menu', 'text', 'border', 'body', 'ruby']
+  elements.forEach((element) => {
+    root.style.setProperty(`--theme-${element}-color`, themes[themeName][element])
+  })
+}
+
+
 const themes = {
   america: {
-    background: 'blue',
-    text: 'red',
-    border: 'black',
+    body: 'transparent',
+    background: '#0000FF',
+    menu: '#0000FF',
+    text: '#FF0000',
+    border: '#FF0000',
+    ruby: '#A52A2A',
   },
   soft: {
-    background: 'pink',
-    text: 'brown',
-    border: 'lightgreen',
+    body: 'transparent',
+    background: '#FFC0CB',
+    menu: '#FFC0CB',
+    text: '#A52A2A',
+    border: '#A52A2A',
+    ruby: '#A52A2A',
   },
   default: {
-    background: 'black',
-    text: 'white',
-    border: 'white',
-  }
+    body: 'transparent',
+    background: '#000000',
+    menu: '#000000',
+    text: '#FFFFFF',
+    border: '#FFFFFF',
+    ruby: '#FF0000',
+  },
+  dahlia: {
+    body: 'transparent',
+    background: '#5F4B8BFF',
+    menu: '#5F4B8BFF',
+    text: '#E69A8DFF',
+    border: '#E69A8DFF',
+    ruby: '#A52A2A',
+  },
+  grey: {
+    body: 'transparent',
+    background: '#6C757D',
+    menu: '#6C757D',
+    text: '#343a40',
+    border: '#343a40',
+    ruby: '#212529',
+  },
+  taipei: {
+    body: 'transparent',
+    background: '#14213d',
+    menu: '#14213d',
+    text: '#fca311',
+    border: '#fca311',
+    ruby: '#fca311',
+  },
+  chicago: {
+    body: 'transparent',
+    background: '#fca311',
+    menu: '#fca311',
+    text: '#14213d',
+    border: '#14213d',
+    ruby: '#14213d',
+  },
+  gradient: {
+    body: 'linear-gradient(180deg, #FF0055 0%, #000066 100%)',
+    menu: 'linear-gradient(180deg, #FF0055 0%, #000066 100%)',
+    background: 'transparent',
+    text: '#FFFFFF',
+    border: '#FFFFFF',
+    ruby: '#000000',
+  },
 }
+
+const closeHamburgerMenu = () => {
+  menu.classList.remove('is-open')
+  hamburger.classList.remove("is-active")
+}
+
+const menu = document.querySelector(".menu")
+const hamburger = document.querySelector(".hamburger");
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("is-active")
+  menu.classList.toggle('is-open')
+})
+
+document.addEventListener('keyup', (event) => {
+  if (event.isComposing || event.key === 'Escape') {
+    closeHamburgerMenu()
+  }
+})
